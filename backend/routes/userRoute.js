16582a78c09dba874ee49da912bccc978342b51f
Aziz Hamdani <azizhamdani7@gmail.com>
2023-05-26 17:44:01 +0500

    change password functionality added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,6 +7,7 @@ const {
   getUser,
   loginStatus,
   updateUser,
+  changePassword,
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -17,5 +18,6 @@ router.get("/logout", logout);
 router.get("/getuser", protect, getUser);
 router.get("/loggedin", loginStatus);
 router.patch("/updateuser", protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
 
 module.exports = router;
