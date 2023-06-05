1fcc460e6905a32fa8f3b12de4c80ef3a0362244
Aziz Hamdani <azizhamdani7@gmail.com>
2023-06-05 17:24:02 +0500

    completed backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
 const errorHandler = require("./middlewares/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -23,6 +24,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 //Routes Middleware
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
 //Routes
 app.get("/", (req, res) => {
