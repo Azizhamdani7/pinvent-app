85963512aed3bbdfe84a6de41031d49e0fc915b6
Aziz Hamdani <azizhamdani7@gmail.com>
2023-06-08 13:30:24 +0500

    Completed sidebar

--- frontend/src/components/sidebar/SidebarItem.js
+++ frontend/src/components/sidebar/SidebarItem.js
@@ -1,21 +1,62 @@
 import { useState } from "react";
 import "./Sidebar.scss";
+import { NavLink } from "react-router-dom";
 import { MdKeyboardArrowRight } from "react-icons/md";
+const activeSublink = ({ isActive }) => (isActive ? "active" : "link");
+const activeLink = ({ isActive }) => (isActive ? "active" : "link");
 
 const SidebarItem = ({ item, isOpen }) => {
-  const [expandMenu, setexpandMenu] = useState(false);
+  const [expandMenu, setExpandMenu] = useState(false);
 
   if (item.childrens) {
     return (
       <div
         className={
           expandMenu ? "sidebar-item s-parent open " : "sidebar-item s-parent"
-        }>
-          .side
+        }
+      >
+        <div className="sidebar-title">
+          <span>
+            {item.icon && <div className="icon">{item.icon}</div>}
+            {isOpen && <div>{item.title}</div>}
+          </span>
+          <MdKeyboardArrowRight
+            size={25}
+            className="arrow-icon"
+            onClick={() => setExpandMenu(!expandMenu)}
+          />
+        </div>
+        <div className="sidebar-content">
+          {item.childrens.map((child, index) => {
+            return (
+              <div key={index} className="s-child">
+                <NavLink to={child.path} className={activeSublink}>
+                  <div className="sidebar-item">
+                    <div className="sidebar-title">
+                      {child.icon && <div className="icon">{child.icon}</div>}
+                      {isOpen && <div>{child.title}</div>}
+                    </div>
+                  </div>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   } else {
-    return <div>sidebarItem</div>;
+    return (
+      <NavLink to={item.path} className={activeLink}>
+        <div className="sidebar-item s-parent">
+          <div className="sidebar-title">
+            <span>
+              {item.icon && <div className="icon">{item.icon}</div>}
+              {isOpen && <div>{item.title}</div>}
+            </span>
+          </div>
+        </div>
+      </NavLink>
+    );
   }
 };
 
