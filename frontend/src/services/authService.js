e2bad034a5d41d4b1d4e19d2026e6a66bad5cc0f
Aziz Hamdani <azizhamdani7@gmail.com>
2023-06-09 16:01:25 +0500

    forgot password functionality done

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -62,3 +62,24 @@ export const logoutUser = async () => {
     toast.error(message);
   }
 };
+
+//forgot password
+export const forgotPassword = async (userData) => {
+  try {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/users/forgotpassword`,
+      userData
+    );
+    toast.success(response.data.message);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
+  }
+};
+
+//Reset Password
+
+
